247fad7b43986ac09dd350827e4dc3477a22cde1
Temesgen Moges <temu1554@gmail.com>
2024-04-05 16:27:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Membership from './screan/Membership';
+
+
+
+const App = () => {
+  return (
+   <dev>
+    <Membership/>
+   </dev>
+
+    
+  );
+}
+
+export default App;
